9adffcc6f173a9826cf1478fb8ef3dd668d7ce8e
pushpabrol <pushp.abrol@gmail.com>
2023-05-15 19:07:28 -0400

    Initial Commit

---
+++ PasskeyLogin/PasskeyLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasskeyLoginApp.swift
+//  PasskeyLogin
+//
+//  Created by Pushp Abrol on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct PasskeyLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
